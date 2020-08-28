b4e6b12a091724639bbae5ac341812a2cbf854a2
Ibraheem Khaleel <ikhaleelem@gmail.com>
2020-08-28 07:00:33 +0530

    add code for unit conversion

---
+++ unitconversion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+inches=12
+fortytwoinches=$((42/inches))
+echo " 42 inches equals $fortytwoinches feet "
+area=60*40
+areainmeters=$((area*9/100))
+echo " area of 60*40 feet equals $areainmeters sqaure meters "
+areaof25plots=$((25*areainmeters))
+oneacre=4046
+acresof25plots=$((areaof25plots/oneacre))
+echo " acres of 25 plot is $acresof25plots acres "
